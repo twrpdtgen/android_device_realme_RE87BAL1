6b5cb87927614156b5f5383399780085ee4441fd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-09-23 09:35:08 +0200

    RE87BAL1: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: realme Model: RMX3511 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_RE87BAL1.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_RE87BAL1-user \
+    omni_RE87BAL1-userdebug \
+    omni_RE87BAL1-eng
